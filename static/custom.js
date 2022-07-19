cf95552b82061a902b33bb536844d88bfadc794d
MarcoFrancisco13 <franciscomarco2921@gmail.com>
2022-07-19 20:15:47 +0800

    Added job card carousel

--- static/custom.js
+++ static/custom.js
@@ -1,9 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.sidenav')
-  var instances = M.Sidenav.init(elems, options)
-})
 
-// Or with jQuery
 
 $(document).ready(function () {
   $('.sidenav').sidenav()
@@ -65,22 +60,6 @@ $('.slider').each(function () {
     
   }
 
-  $('.next_btn').on('click', function () {
-    if (currentIndex < $slides.length - 1) {
-      move(currentIndex + 1)
-    } else {
-      move(0)
-    }
-  })
-
-  $('.previous_btn').on('click', function () {
-    if (currentIndex !== 0) {
-      move(currentIndex - 1)
-    } else {
-      move(3)
-    }
-  })
-
   $.each($slides, function (index) {
     var $button = $('<a class="slide_btn">&bull;</a>')
 
@@ -92,6 +71,80 @@ $('.slider').each(function () {
         move(index)
       })
       .appendTo('.slide_buttons')
+    bulletArray.push($button)
+  })
+
+  advance()
+})
+
+
+$('.jobSlider').each(function () {
+  var $this = $(this)
+  var $group = $this.find('.job_slide_group')
+  var $slides = $this.find('.jobSlide')
+  var bulletArray = []
+  var currentIndex = 0
+  var timeout
+
+  function move(newIndex) {
+    var animateLeft, slideLeft
+
+    advance()
+
+    if ($group.is(':animated') || currentIndex === newIndex) {
+      return
+    }
+
+    bulletArray[currentIndex].removeClass('active')
+    bulletArray[newIndex].addClass('active')
+
+    if (newIndex > currentIndex) {
+      slideLeft = '100%'
+      animateLeft = '-100%'
+    } else {
+      slideLeft = '-100%'
+      animateLeft = '100%'
+    }
+
+    $slides.eq(newIndex).css({
+      display: 'block',
+      left: slideLeft,
+    })
+    $group.animate(
+      {
+        left: animateLeft,
+      },
+      function () {
+        $slides.eq(currentIndex).css({
+          display: 'none',
+        })
+        $slides.eq(newIndex).css({
+          left: 0,
+        })
+        $group.css({
+          left: 0,
+        })
+        currentIndex = newIndex
+      },
+    )
+  }
+
+  function advance() {
+    clearTimeout(timeout)
+    
+  }
+
+  $.each($slides, function (index) {
+    var $button = $('<a class="slide_btn">&bull;</a>')
+
+    if (index === currentIndex) {
+      $button.addClass('active')
+    }
+    $button
+      .on('click', function () {
+        move(index)
+      })
+      .appendTo('.job_slide_buttons')
     bulletArray.push($button)
   })
 
