8e412841fb7896805e40f7127e40f03531fe49c3
MarcoFrancisco13 <franciscomarco2921@gmail.com>
2022-07-20 11:52:01 +0800

    Added Top Nav Bar

--- static/custom.js
+++ static/custom.js
@@ -1,4 +1,11 @@
-
+function openNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
 
 $(document).ready(function () {
   $('.sidenav').sidenav()
